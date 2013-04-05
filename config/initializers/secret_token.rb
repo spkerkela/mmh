44db2e82f2263a11aae3021b10ef9edbbd3903f2
Simo-Pekka Kerkelä <simo@simo-HP-EliteBook-2560p.(none)>
2013-04-05 13:42:04 +0300

    My initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviemarathonhelper::Application.config.secret_token = 'baf3025073282d61cc9bd50321f80fd32da4615cead6e589e42bd0d3cc9fba741a2e2b367488bfe099fa358e43db93798cfd4473de930037a62787f00367b65f'
