7e645b678843425c4c38e8b23119419550843c66
lauri kääriäinen <lauri@gayburger.local>
2013-05-11 21:25:45 +0300

    finished user_controller_tests //lauri

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -5,31 +5,43 @@ class UsersControllerTest < ActionController::TestCase
     @request.session[:user_id] = user ? user.id : nil 
   end 
   
-   test "should get new" do
+  test "should get new" do
     get :new 
     assert :success
   end
   
   test "should create a user" do
+     
       assert_difference "User.count" do
         post :create, user: {name: "new_name", email: "new_email@email.com", password: "12341234",
-                      password_confirmation: "12341234"
-                     }
+                      password_confirmation: "12341234"}
       end
       assert_equal "Account created", flash.now[:success]
-      assert_redirected_to user_path(users(:one))
+      assert :redirect
   end
   
   
-   test "should not create a user" do
+  test "should not create a user because of non-unique name" do
       assert_difference "User.count", 0 do
-        post :create, user: {name: users(:one).name, email: users(:one).email, password: "12341234",
+        post :create, user: {name: users(:one).name, email: "testi@testi.com", password: "12341234",
                       password_confirmation: "12341234"
                      }
       end
       assert_equal "There are some issues with your form", flash.now[:alert]
       assert :new
   end
+  
+  test "should not create a user because of non-unique email" do
+      assert_difference "User.count", 0 do
+        post :create, user: {name: "some_name", email: users(:one).email, password: "12341234",
+                      password_confirmation: "12341234"
+                     }
+      end
+      assert_equal "There are some issues with your form", flash.now[:alert]
+      assert :new
+  end
+  
+  
   # test "the truth" do
   #   assert true
   # end
