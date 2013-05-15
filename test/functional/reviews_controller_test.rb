7ad5433cf9a7448f9eaa0e454defa5a327d936eb
lauri kääriäinen <lauri@gayburger.local>
2013-05-15 13:40:15 +0300

    done review controller tests and unit tests //lauri

--- test/functional/reviews_controller_test.rb
+++ test/functional/reviews_controller_test.rb
@@ -1,6 +1,41 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
+   def setup 
+    @user = User.new(
+                     name: "erisnimi",
+                     email: "example@ext.com",
+                     password: "12341234",
+                     password_confirmation: "12341234")
+
+    @user.save
+    
+    @movie = movies(:one)
+    
+  end
+
+  def login_as(user)
+    @request.session[:user_id] = user ? user.id : nil
+  end
+
+  
+  test "should get new review" do
+    login_as(@user)
+    get :new, movie_id: @movie.id
+    assert_response :success
+  end
+  
+
+  
+  test "should create a review" do
+    login_as(@user)
+ #  review1 = FactoryGirl.create(:review)
+ #   post :create, review: {movie_id: review1.movie_id, user_id: review1.user_id, rating: review1.rating, content: review1.content}
+    post :create, review: {movie_id: 1, user_id: 1, rating: 1.5, content: "testireview"}
+    assert_equal "Review created", flash.now[:success]
+    assert_redirected_to movies_path
+  end
+ 
   # test "the truth" do
   #   assert true
   # end
--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -14,6 +14,53 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
+  test "should save with valid inputs" do
+    review = Review.new
+    review.id = 1
+    review.user_id = 1
+    review.movie_id = 1
+    review.rating = 5
+    review.content ="ihan ok ja jees"
+    assert review.valid?
+    assert review.save
+    
+  end
+  
+  test "should not save with same user_id and movie_id" do
+    review = Review.new
+    review.id = 1
+    review.user_id = 1
+    review.movie_id = 1
+    review.rating = 5
+    review.content ="ihan ok ja jees"
+    review.save
+    
+    review2 = Review.new
+    review2.id = 2
+    review2.user_id = 1
+    review2.movie_id = 1
+    review2.rating = 5
+    review2.content ="ihan ok ja jees"
+    
+    assert review2.invalid?
+    assert !review2.save
+    
+  end
+  
+  test "should not save without user_id or rating" do
+    review = Review.new
+    review.id = 1
+    #review.user_id = 1
+    review.movie_id = 1
+    #review.rating = 5
+    review.content ="ihan ok ja jees"
+    assert review.invalid?
+    assert !review.save
+    
+  end
+  
+  
+  
   # test "the truth" do
   #   assert true
   # end
