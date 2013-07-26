a217092bd4fe4f15f1f1524c0c01e0ca09277464
Simo-Pekka Kerkelä <maurikerkela@Mauri-Kerkela-MacBook-Pro.local>
2013-07-26 17:33:12 +0300

    README update

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20130514192206) do
     t.integer  "user_id"
   end
 
+  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
+
   create_table "movies", :force => true do |t|
     t.string   "title"
     t.string   "year"
